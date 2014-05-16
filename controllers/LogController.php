5cc33219689715184380dbd938cc1e70551574ec
cakebake (Jens A.) <cakebake.dev@gmail.com>
2014-05-17 01:05:07 +0200

    Readd method findModel()

--- controllers/LogController.php
+++ controllers/LogController.php
@@ -6,6 +6,7 @@ use Yii;
 use cakebake\actionlog\model\ActionLog;
 use cakebake\actionlog\model\ActionLogSearch;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 use yii\filters\AccessControl;
 
 /**
@@ -54,5 +55,21 @@ class LogController extends Controller
         return $this->render('view', [
             'model' => $this->findModel($id),
         ]);
+    }
+
+    /**
+     * Finds the ActionLog model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $id
+     * @return ActionLog the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = ActionLog::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
     }
 }

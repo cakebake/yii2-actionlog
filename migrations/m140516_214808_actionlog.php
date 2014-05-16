5f1caf12f419341aa07f5a2217e0a39550416348
cakebake (Jens A.) <cakebake.dev@gmail.com>
2014-05-17 00:10:20 +0200

    Added database schema

---
+++ migrations/m140516_214808_actionlog.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Schema;
+
+/**
+* DATABSE SCHEMA OF THIS MODULE
+*
+* @version 1.0.0
+* @see http://www.yiiframework.com/doc-2.0/guide-console-migrate.html
+*/
+class m140516_214808_actionlog extends \yii\db\Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%actionlog}}', [
+            'id' => Schema::TYPE_BIGPK,
+            'user_id' => Schema::TYPE_BIGINT . ' NOT NULL DEFAULT 0',
+            'user_remote' => Schema::TYPE_STRING . ' NOT NULL',
+            'time' => Schema::TYPE_TIMESTAMP . ' NOT NULL',
+            'action' => Schema::TYPE_STRING . ' NOT NULL',
+            'category' => Schema::TYPE_STRING . ' NOT NULL',
+            'message' => Schema::TYPE_TEXT . ' NOT NULL',
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%actionlog}}');
+    }
+}

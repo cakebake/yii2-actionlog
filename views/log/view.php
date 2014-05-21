4fa3c7e665bff080c01a5a9af17ebe6802a49575
cakebake (Jens A.) <cakebake.dev@gmail.com>
2014-05-21 23:51:30 +0200

    Reader-friendly detailed view

--- views/log/view.php
+++ views/log/view.php
@@ -2,31 +2,43 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\helpers\VarDumper;
 
 /**
  * @var yii\web\View $this
  * @var cakebake\actionlog\model\ActionLog $model
  */
 
-$this->title = Yii::t('actionlog', 'Action Log') . ' ' . Yii::t('actionlog', 'ID') . $model->id;
+$this->title = Yii::t('actionlog', 'Action Log') . ' #' . $model->id;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('actionlog', 'Action Logs'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="action-log-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->title) ?> <small><?= Html::encode($model->category) ?>/<?= Html::encode($model->action) ?></small></h1>
 
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
             'id',
+            'time',
+            'category',
+            'action',
             'user_id',
             'user_remote',
-            'time',
-            'action',
-            'category',
-            'message:ntext',
         ],
     ]) ?>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h3 class="panel-title"><?= Yii::t('actionlog', 'Message') ?></h3>
+        </div>
+        <div class="panel-body">
+            <?php VarDumper::dump(@unserialize($model->message), 10, true) ?>
+        </div>
+        <ul class="list-group">
+            <li class="list-group-item text-muted"><?= Html::encode($model->message) ?></li>
+        </ul>
+    </div>
 
 </div>
